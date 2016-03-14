9c3fae63b8c83b587d532a5d32b65eceaff76c06
dylan-wright <dcwr222@uky.edu>
2016-03-14 09:44:55 -0400

    ModelForms

--- editor/admin.py
+++ editor/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import *
 
 # Register your models here.
+admin.site.register(Character)
+admin.site.register(Location)
+admin.site.register(Event)
+admin.site.register(Description)
+admin.site.register(DescribedBy)
+admin.site.register(HappenedAt)
+admin.site.register(Involved)
---
+++ editor/forms.py
@@ -0,0 +1,37 @@
+from django import forms
+from .models import *
+
+class EventForm(forms.ModelForm):
+    class Meta:
+        model = Event
+        fields = ('turn', 'notes')
+
+class DescriptionForm(forms.ModelForm):
+    class Meta:
+        model = Description
+        fields = ('text', 'hidden', 'key', 'name')
+
+class DescribedByForm(forms.ModelForm):
+    class Meta:
+        model = DescribedBy
+        fields = ('event', 'description')
+
+class HappenedAtForm(forms.ModelForm):
+    class Meta:
+        model = HappenedAt
+        fields = ('event', 'location')
+
+class CharacterForm(forms.ModelForm):
+    class Meta:
+        model = Character
+        fields = ('name', 'key', 'notes')
+
+class InvolvedForm(forms.ModelForm):
+    class Meta:
+        model = Involved
+        fields = ('event', 'character')
+
+class LocationForm(forms.ModelForm):
+    class Meta:
+        model = Location
+        fields = ('name', 'x', 'y')

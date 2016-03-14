ac550928ef1112b70a5224a692cd90fd40e1e52a
dylan-wright <dcwr222@uky.edu>
2016-03-13 21:36:05 -0400

    urls

--- intell/urls.py
+++ intell/urls.py
@@ -17,5 +17,7 @@ from django.conf.urls import url
 from django.contrib import admin
 
 urlpatterns = [
+    url(r'^editor/', include('editor.urls')),
+    url(r'^game/', include('game.urls')),
     url(r'^admin/', admin.site.urls),
 ]

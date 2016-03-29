a3a90560cad05aacb562c71c5362156e7b83f392
caseyokane <casey.okane@uky.edu>
2016-03-29 16:23:47 -0400

    Initial design for my js file

---
+++ editor/static/editor/FrontEndtoJSON.js
@@ -0,0 +1,16 @@
+function FrontEndtoJSON() {
+
+//When page is changed temprorarily store objects with a basic format
+//determined by whatever page is ebing used.
+
+    //Search to see if change with that ID specified by the format. If so
+    //replace that entry. If not, append it and leave it alone
+
+//After this temporary save occurs switch the view to match that of a different
+//editor page (as specified by the user). 
+
+//Repeat this until final change occurs and user submits all changes at which
+//point the temporary objects will be appended to the JSON file and then sent
+//to receiveAJAX
+
+}

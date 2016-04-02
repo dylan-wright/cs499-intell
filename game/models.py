ffed6b3d5eb7bba5f7ce82e2d39bac2cade3f89f
Dylan Wright <dylan.wright@uky.edu>
2016-04-01 23:51:25 -0400

    game models

--- game/models.py
+++ game/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from editor.models import Scenario
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Player(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+class Game(models.Model):
+    scenario = models.ForeignKey(Scenario, on_delete=models.CASCADE)
+    players = models.ManyToManyField(Player)

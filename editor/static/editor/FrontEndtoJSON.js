a28f54b4e7ed7becbe3bd12c264ce31e7e1eaddf
caseyokane <casey.okane@uky.edu>
2016-04-03 11:34:17 -0400

    Updating toJSONClass to use in the scenario editor. Still needs some work concerning the keys though.

--- editor/static/editor/FrontEndtoJSON.js
+++ editor/static/editor/FrontEndtoJSON.js
@@ -1,16 +1,194 @@
-function FrontEndtoJSON() {
+/*
+  The toJSONClass() will be used to instantiate an object in the scenario editor
+  HTML given values to replace the name, turn, point and author. After it is 
+  instantiated, it's method will be called whenever an onclick even occurs 
+  to one of it's related buttons. After this occurs, the hashmap hashJSON
+  will be updated with the new objects. Once the submit method is used, the 
+  JSON file will be formed using the hashmap and then sent to the back end.
+*/
 
-//When page is changed temprorarily store objects with a basic format
-//determined by whatever page is ebing used.
+function toJSONClass() {
 
-    //Search to see if change with that ID specified by the format. If so
-    //replace that entry. If not, append it and leave it alone
+    //Scenario properties
+    this.name = 'NULL';
+    this.turn_num = 20;
+    this.point_num = 20;
+    this.author = 'NULL';
 
-//After this temporary save occurs switch the view to match that of a different
-//editor page (as specified by the user). 
+    //hashMap to contain input, passed from 
+    this.hashJSON = [];
 
-//Repeat this until final change occurs and user submits all changes at which
-//point the temporary objects will be appended to the JSON file and then sent
-//to receiveAJAX
+    //Character related methods
+    this.add_char = function(charName, isKey, charNotes) {
+
+        //Create a character object to match with the fixture.json format
+        var charObj = {
+            model:"editor.character",
+            pk:42,
+            fields:{
+                name: charName,
+                key: isKey,
+                notes: charNotes
+            }
+        };
+
+        //Add the character object to the hashmap where the pk will be used
+        //to determine this objects location
+        this.hashJSON[pk] = charObj;
+
+    }
+
+    this.edit_char = function(charName, isKey, charNotes) {
+        
+        //check that the entry already exists
+        if(pk in hashJSON){
+                //Use key value to locate the object in the hashmap and then set  
+                //it to a new object using the hashmap
+                hashJSON[pk] = {
+                    model:"editor.character",
+                    pk:42,
+                    fields:{
+                        name: charName,
+                        key: isKey,
+                        notes: charNotes
+                    }
+                };
+        }
+
+    }
+
+    this.del_char = function(charName, isKey, charNotes) {
+        //Check that the key is in hashJSON and delete it if so. 
+        if(pk in hashJSON){
+            delete hashJSON[pk];
+        }
+    }
+
+    //Event related methods
+    this.add_event = function(eventName, isKey, isSecret, eventSnip, tagTurn,
+    tagType, tagTarget) {
+        
+        //Create an event object to match with the fixture.json format
+        var eventObj = {
+            model:"editor.event",
+            pk:42,
+            fields:{
+                name: eventName,
+                key: isKey,
+                secret: isSecret,
+                snippet: eventSnip,
+                turn: tagTurn,
+                type: tagType,
+                target: tagTarget
+            }
+        };
+
+        //Add the character object to the hashmap where the pk will be used
+        //to determine this objects location
+        hashJSON[pk] = eventObj;
+    }
+
+    this.edit_event = function(eventName, isKey, isSecret, eventSnip, tagTurn,   
+    tagType, tagTarget) {
+        
+        if(pk in hashJSON){
+            hashJSON[pk] = {
+                model:"editor.event",
+                pk:42,
+                fields:{
+                    name: eventName,
+                    key: isKey,
+                    secret: isSecret,
+                    snippet: eventSnip,
+                    turn: tagTurn,
+                    type: tagType,
+                    target: tagTarget
+                }
+            };
+        }
+    }
+
+    this.del_event = function(eventName, isKey, isSecret, eventSnip, tagTurn,   
+    tagType, tagTarget) {
+        
+        if(pk in hashJSON){
+            delete hashJSON[pk];
+        }
+    }
+
+    //Location related methods
+    this.add_loc = function(locName, locCoordX, locCoordY) {
+        
+        //Create a location object to match with the fixture.json format
+        var locObj = {
+            model:"editor.location",
+            pk:42,
+            fields:{
+                name: locName,
+                x: locCoordX,
+                y: locCoordY
+            }
+        };
+
+        hashJSON[pk] = locObj;
+
+    }
+
+    this.edit_loc = function(locName, locCoordX, locCoordY) {
+       
+        //check that the entry already exists
+        if(pk in hashJSON){
+                this.hashJSON[pk] = {
+                    model:"editor.location",
+                    pk:42,
+                    fields:{
+                        name: locName,
+                        x: locCoordX,
+                        y: locCoordY
+                    }
+                };
+        }
+
+    }
+
+    this.del_loc = function(locName, locCoordX, locCoordY) {
+        
+        if(pk in hashJSON){
+            delete hashJSON[pk];
+        }
+    }
+
+
+    //Final method to submit the JSON currently stored in hashJSON
+    this.submitJSON = function(){
+        
+        //Create a final array tat will contain the JSON objects
+        var JSONarr = [];
+
+        //Iterate through each object in the hashMap
+        for(var key in hashJSON){
+            
+            //Get the key values of the hashmap
+            var value = hashJSON[key];
+
+
+            //Iterate through each filed in the value of the hashmap
+            for(var field in value){ 
+                //push each object to the JSON array
+                JSONarr.push(value[field]);
+            }
+
+        }
+
+        //Generate the JSON file using stringify on the JSON array
+        //after the hashmap has been iterated through
+        var JSONfile = JSON.stringify(JSONarr);
+
+
+    }
+
+
 
 }
+
+
